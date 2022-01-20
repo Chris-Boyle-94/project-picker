9a13fadda6ca820aed5a7c4ec6ca59188e0f1f16
Christopher Boyle <cboyle824@gmail.com>
2022-01-20 11:44:06 -0500

    added a component to display a random API

---
+++ src/components/ApiRandom.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import ApiCard from "./ApiCard";
+
+const ApiRandom = ({ apis }) => {
+    const randomStart = Math.floor(Math.random() * (apis.length + 1));
+    const [selectedApi, setSelectedApi] = useState(apis[randomStart]);
+
+    const handleClick = () => {
+        const randomNum = Math.floor(Math.random() * (apis.length + 1));
+        setSelectedApi(apis[randomNum]);
+    };
+
+    console.log(selectedApi);
+
+    return (
+        <div>
+            <ApiCard api={selectedApi} />
+            <button onClick={handleClick}>Give me a random api</button>
+        </div>
+    );
+};
+
+export default ApiRandom;
